2ea787a7f1b1fabd6a9d4383a85103957cf136d0
Maria <mariabrudniak@gmail.com>
2025-10-30 16:40:05 +0000

    Initial Commit

---
+++ Baseline/BaselineApp.swift
@@ -0,0 +1,17 @@
+//
+//  BaselineApp.swift
+//  Baseline
+//
+//  Created by maria on 30/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct BaselineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BaselineTests/BaselineTests.swift
@@ -0,0 +1,17 @@
+//
+//  BaselineTests.swift
+//  BaselineTests
+//
+//  Created by maria on 30/10/2025.
+//
+
+import Testing
+@testable import Baseline
+
+struct BaselineTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
